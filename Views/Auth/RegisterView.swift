27e22a377441d7d882b3b73d106c7a48e0423550
UsaBem Developer <dev@usabem.com>
2026-09-16 09:22:56 -0300

    Fix: Remove unused Triangle struct causing build conflict

--- Views/Auth/RegisterView.swift
+++ Views/Auth/RegisterView.swift
@@ -1,18 +1,6 @@
 import SwiftUI
 import PhotosUI
 
-// Triangle shape used for map pin tail
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        path.move(to: CGPoint(x: rect.midX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
-        path.closeSubpath()
-        return path
-    }
-}
-
 struct RegisterView: View {
     @StateObject private var viewModel = RegisterViewModel()
     @EnvironmentObject var authViewModel: AuthViewModel
