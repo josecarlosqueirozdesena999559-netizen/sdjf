8ed07f633ae05ecb57818568521c7e898556c455
UsaBem Developer <dev@usabem.com>
2026-09-17 13:29:14 -0300

    Fix: Remove force unwrap in MessagesViewModel

--- ViewModels/MessagesViewModel.swift
+++ ViewModels/MessagesViewModel.swift
@@ -6,8 +6,9 @@ class MessagesViewModel: ObservableObject {
     
     func fetchConversations() {
         // Mock data
-        let sampleProduct = MockData.products.first!
+        guard let sampleProduct = MockData.products.first else { return }
         let msg = Message(id: UUID(), senderId: UUID(), receiverId: UUID(), text: "Olá, o produto ainda está disponível?", timestamp: Date(), isRead: false)
+        guard MockData.users.count > 1 else { return }
         let conv = Conversation(id: UUID(), productId: sampleProduct.id, participantId: MockData.users[1].id, lastMessage: msg, unreadCount: 1)
         
         self.conversations = [conv]
